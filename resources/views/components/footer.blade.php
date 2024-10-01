dfc4602d8025aa1cb71dac845f686bf61938ffc6
Alazar Ab <abraham1990alazar@gmail.com>
2024-10-01 21:49:39 +0300

    Intial commit on main 20241001 - Project FCY Registration intialization

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>All Rights Reserved</span>
+        <span>2024</span>
+        <a
+            href="https://www.siketbank.com"
+            target="_blank"
+            class="text-blue-300 hover:underline"
+        >
+            Siket Bank
+        </a>
+    </p>
+</footer>
